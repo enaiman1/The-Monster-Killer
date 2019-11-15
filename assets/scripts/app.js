4b9993a3c6440ad0fd61e1e82c13ed9502d25d9f
Ericcentrik <enaiman1@gmail.com>
2019-11-15 16:38:54 -0500

    refactored attack and stong attack function by creating a new function with conditionals

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,32 +8,31 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler() {
-const damage = dealMonsterDamage(ATTACK_VALUE);
-currentMonsterHealth -= damage;
-const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-currentPlayerHealth -= playerDamage;
-if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-    alert("You won!")
-}else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
-    alert("You lost!")
-}else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ) {
-    alert("its a draw")
-}
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === "ATTACK") {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === "STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("You won!")
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("You lost!")
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert("its a draw")
+    }
 }
 
-function strongAttackHandler(){
-    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-currentMonsterHealth -= damage;
-const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-currentPlayerHealth -= playerDamage;
-if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-    alert("You won!")
-}else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
-    alert("You lost!")
-}else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ) {
-    alert("its a draw")
+function attackHandler() {
+    attackMonster("ATTACK");
 }
+function strongAttackHandler() {
+    attackMonster("STRONG_ATTACK");
 }
 
 attackBtn.addEventListener('click', attackHandler);
