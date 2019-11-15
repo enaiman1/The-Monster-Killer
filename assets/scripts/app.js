db660a7745f7ce258d774a10812b185428dc1c85
Ericcentrik <enaiman1@gmail.com>
2019-11-15 17:31:35 -0500

    created healPlayerHandler function and moved health check conditional to endRound function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,23 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAl_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
+
+function endRound(){
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("You won!")
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("You lost!")
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert("its a draw")
+    }
+}
 
 function attackMonster(mode) {
     let maxDamage;
@@ -19,13 +30,7 @@ function attackMonster(mode) {
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert("You won!")
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert("You lost!")
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-        alert("its a draw")
-    }
+  endRound();
 }
 
 function attackHandler() {
@@ -35,5 +40,12 @@ function strongAttackHandler() {
     attackMonster("STRONG_ATTACK");
 }
 
+function healPlayerHandler(){
+increasePlayerHealth(HEAl_VALUE);
+endRound()
+}
+
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
