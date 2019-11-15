da74b5f0db1d9c9ff6badacefd2ac5706960235a
Ericcentrik <enaiman1@gmail.com>
2019-11-15 16:29:49 -0500

    added a new function to deal more damage

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,7 @@
-const ATTACK_VALUE = 10
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
@@ -8,9 +11,30 @@ adjustHealthBars(chosenMaxLife)
 function attackHandler() {
 const damage = dealMonsterDamage(ATTACK_VALUE);
 currentMonsterHealth -= damage;
-if(currentMonsterHealth <= 0 ){
+const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+currentPlayerHealth -= playerDamage;
+if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
     alert("You won!")
+}else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+    alert("You lost!")
+}else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ) {
+    alert("its a draw")
+}
+}
+
+function strongAttackHandler(){
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+currentMonsterHealth -= damage;
+const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+currentPlayerHealth -= playerDamage;
+if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+    alert("You won!")
+}else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+    alert("You lost!")
+}else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ) {
+    alert("its a draw")
 }
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
