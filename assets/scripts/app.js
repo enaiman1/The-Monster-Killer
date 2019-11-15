7644eb6a20ad9d89424bbe7d2d5f0ed0d5f144ad
Ericcentrik <enaiman1@gmail.com>
2019-11-15 18:21:58 -0500

    created reset functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,12 @@ let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife)
 
+function reset() {
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+  }
+
 function endRound(){
     const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -29,6 +35,10 @@ function endRound(){
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
         alert("its a draw")
     }
+
+    if ( currentMonsterHealth <= 0 || currentPlayerHealth <= 0 ) {
+            reset();
+        }
 }
 
 function attackMonster(mode) {
