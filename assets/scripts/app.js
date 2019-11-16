4a85896910a8e6731d26c1c8a76c25a14e37de5f
Ericcentrik <enaiman1@gmail.com>
2019-11-16 09:30:51 -0500

    converted string identifiers into global variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,14 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAl_VALUE = 20;
 
+const MODE_ATTACK = 'ATTACK'; //MODE_ATTAK = 0
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK';//MODE_STRONG_ATTACK =1
+
 const enterValue = prompt('Maxium life for you and the monster', '100');
 
 let chosenMaxLife = parseInt(enterValue);
 
-if(isNAN (chosenMaxLife) || chosenMaxLife <= 0){
+if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
     chosenMaxLife = 100;
 }
 
@@ -52,9 +55,9 @@ function endRound(){
 
 function attackMonster(mode) {
     let maxDamage;
-    if (mode === "ATTACK") {
+    if (mode === MODE_ATTACK) {
         maxDamage = ATTACK_VALUE;
-    } else if (mode === "STRONG_ATTACK") {
+    } else if (mode === MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE;
     }
     const damage = dealMonsterDamage(maxDamage);
@@ -63,10 +66,10 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-    attackMonster("ATTACK");
+    attackMonster(MODE_ATTACK);
 }
 function strongAttackHandler() {
-    attackMonster("STRONG_ATTACK");
+    attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler(){
